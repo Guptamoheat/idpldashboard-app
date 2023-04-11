9bd1c4870b2ad121b625961b8d1c4f0271ef7a0c
Guptamoheat <guptamohit801@gmail.com>
2023-04-11 22:02:33 +0530

    Completed UI elements for Team Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import { MatchPage } from './pages/MatchPage';
 
 import { TeamPage } from './pages/TeamPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
+
   return (
     <div className="App">
-      <TeamPage/>
+
+      <BrowserRouter>
+      <Routes>
+          <Route path="/teams/:teamName" element = {<TeamPage></TeamPage>}/>
+          <Route path="/teams/:teamName/matches/:year" element = {<MatchPage></MatchPage>}/>
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
--- src/pages/TeamPage.jsx
+++ src/pages/TeamPage.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { MatchDetailCard } from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
+import { useParams } from "react-router-dom";
 
 export const TeamPage = () => {
 
     const [team, setTeam] = useState({});
     const [matches, setMatches] = useState([])
-    
+    const {teamName} = useParams();
     useEffect(
         () => {
             const fetchMatches = async () => {
-                    const response = await fetch('http://localhost:8080/team/Rajasthan Royals');
+                    const response = await fetch(`http://localhost:8080/team/${teamName}`);
                     const data = await response.json();
                     setTeam(data);
                     setMatches(data.matches)
@@ -19,15 +20,17 @@ export const TeamPage = () => {
             fetchMatches();
             
         },
-        []
+        [teamName]
     )
 
-
+    if(!team || !team.teamName) {
+        return <><h1>Team Not Found</h1></>
+    }
     return (
         <div className="TeamPage">
             <h1>{team.teamName}</h1>
-            <MatchDetailCard match= {matches[0]}/>
-            {matches.slice(1).map(match =>  < MatchSmallCard match={match}/>)}
+            <MatchDetailCard teamName = {team.teamName} match= {matches[0]}/>
+            {matches.slice(1).map(match =>  < MatchSmallCard teamName = {team.teamName} match={match}/>)}
         </div>
     )
 }
