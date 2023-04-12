ed62b14daa6ff42e0d69dad7c0c22851652583a1
Guptamoheat <guptamohit801@gmail.com>
2023-04-12 11:24:09 +0530

    Team Page Completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import { MatchPage } from './pages/MatchPage';
 
 import { TeamPage } from './pages/TeamPage';
--- src/pages/TeamPage.jsx
+++ src/pages/TeamPage.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { MatchDetailCard } from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
 import { useParams } from "react-router-dom";
+import './TeamPage.scss'
+import { PieChart } from 'react-minimal-pie-chart';
 
 export const TeamPage = () => {
 
@@ -20,17 +22,43 @@ export const TeamPage = () => {
             fetchMatches();
             
         },
-        []
+        [teamName]
     )
+
+    
+    const losses = team.totalMatches - team.totalWins;
+
+    
 
     if(!team || !team.teamName) {
         return <><h1>Team Not Found</h1></>
     }
     return (
         <div className="TeamPage">
-            <h1>{team.teamName}</h1>
-            <MatchDetailCard teamName = {team.teamName} match= {matches[0]}/>
+            <div className="team-name-section">
+                <h1 className="team-name">{team.teamName}</h1>
+            </div>
+            
+            <div className="win-loss-section">
+                Win /Losses
+                <PieChart
+  data={[
+    { title: 'Wins', value: team.totalWins, color: '#4da375' },
+    { title: 'Losses', value: losses, color: '#a34d5d' },
+  ]}
+/>
+            </div>
+
+
+            <div className="match-detail-section">
+                <h3>Latest Match</h3>
+                <MatchDetailCard teamName = {team.teamName} match= {matches[0]}/>
+            </div>
             {matches.slice(1).map(match =>  < MatchSmallCard teamName = {team.teamName} match={match}/>)}
+        
+            <div className="more-link">
+                <a href="#" >More</a>
+            </div>
         </div>
     )
 }
